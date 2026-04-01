6543b95a424e66a7922902525c3a7e17f84535e9
Csenge Hubay <csengehubay@gmail.com>
2026-04-01 17:04:39 +0200

    Initial commit

---
+++ tiago_custom_nav2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'tiago_custom_nav2'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        # ADD THESE LINES TO INSTALL YOUR DIRECTORIES:
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'module'), glob('module/*')), 
+    ],
+    
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Csenge Hubay',
+    description='TIAGO custom nav2 package',
+    license='Apache License 2.0',
+
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
